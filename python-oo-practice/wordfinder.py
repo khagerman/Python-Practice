1002dc8990031ca8a8e1945d3baffc4faf09a1b0
khagerman <71734063+khagerman@users.noreply.github.com>
2021-03-31 11:33:11 -0500

    saving files so far

--- python-oo-practice/wordfinder.py
+++ python-oo-practice/wordfinder.py
@@ -19,10 +19,11 @@ import random
 
 class WordFinder:
     def __init__(self, file_name="words.txt"):
-    """get file to read 
-    get words from file
-    print number of words read in file
-    """
+
+        # get file to read
+        # get words from file
+        # print number of words read in file
+
         self.file_name = file_name
         self.words = open(self.file_name).read().splitlines()
         print(f"{len(self.words)} words read")
@@ -30,9 +31,26 @@ class WordFinder:
     def random(self):
         """Return random word from file given"""
         return random.choice(self.words)
+
     # def __repr__(self):
     #     "Show reprenstation"
     #     return f"<start={self.start} current_num={self.current_num}>"
 
-    class SpecialWordFinder(WordFinder):
-        def check_if_word(self, file_name)
+
+class SpecialWordFinder(WordFinder):
+    """
+    Method that returns a random word from a list of words but omits blank lines
+    or words that begin with a special character.
+    """
+
+    def __init__(self, words):
+        # get parent class [`super()`], call its `__init__()`
+        super().__init__(words)
+        self.filter_words = [
+            word for word in self.words if word != "" and not word.startswith("#")
+        ]
+        print(f"{len(self.filter_words)} words read")
+
+    def check_if_word(self):
+        for word in self.filter_words:
+            return word
